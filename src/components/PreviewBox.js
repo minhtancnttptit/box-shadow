7bf2662725fb7fd8c738d51365272a52f26a9d14
minhtancnttptit <minhtancnttptit@gmail.com>
2020-05-15 10:59:42 +0700

    thieu dragable

---
+++ src/components/PreviewBox.js
@@ -0,0 +1,45 @@
+import React from "react";
+import {SketchPicker} from "react-color";
+
+class PreviewBox extends React.Component {
+  state = {};
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    if (nextProps !== undefined) {
+      return {boxShadowCss: nextProps.boxShadowCss};
+    }
+    return {};
+  }
+
+    render() {
+        return (
+          <>
+            <style jsx={"true"}>
+              {
+                `
+                  .abc {
+                    box-shadow: ${this.state.boxShadowCss};
+                    width: 200px;
+                    height: 200px;
+                    background: ${this.state.background};
+                  }
+                `
+              }
+            </style>
+            <div className="ui segment">
+              <div style={{ display: "grid", gridTemplateColumns: "repeat(2, 2fr)", gridGap: 20 }}>
+                <div>
+                  <h2>Preview</h2>
+                  <div className="preview-box abc" />
+                </div>
+                <div>
+                  <SketchPicker color="ff0000" onChangeComplete={color => this.setState({'background': color.hex})} />
+                </div>
+              </div>
+            </div>
+          </>
+        );
+    }
+}
+
+export default PreviewBox;
---
+++ src/components/RangeSlider.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+class RangeSlider extends React.Component {
+    state = {value: 0}
+
+    static getDerivedStateFromProps(nextProps, prevState) {
+        if (nextProps.value !== undefined) {
+            return {value: nextProps.value};
+        }
+        return {value: 0};
+    }
+
+    onChangeSlider = (event) => {
+        this.setState({value: event.target.value});
+        this.props.onChange(this.props.name, event.target.value);
+    }
+
+    render() {
+        return (
+            <>
+                <style jsx="true">
+                    {
+                        `
+                        .Slider {
+                            width: 300px
+                        }
+                        `
+                    }
+                </style>
+                <div>
+                    <div className="label">
+                        <label>{this.props.name}</label>
+                    </div>
+                    <div className="RangeSlider ">
+                        <input type="range" className="Slider" id="Slider" step={1}
+                               aria-valuemax={this.props.max} aria-valuemin={this.props.min} min={this.props.min}
+                               max={this.props.max}  value={this.state.value} onChange={e => this.onChangeSlider(e)}/>
+                    </div>
+                </div>
+            </>
+
+        );
+    }
+}
+
+export default RangeSlider;
